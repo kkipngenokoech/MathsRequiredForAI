bb1fc9901d5ea2abd211cec22c1192d5ef506b8e
kipngeno koech <kkipngenokoech22@gmail.com>
2024-09-12 07:42:31 +0200

    cards

--- latex/cards.py
+++ latex/cards.py
@@ -26,10 +26,40 @@ class Card:
         return self.__str__()
     
 deck = DeckCard()
+deck.shuffle()
 
 '''
 drawing 10 cards from the deck, without replacement
 '''
 drawn_cards = [deck.draw() for _ in range(10) if deck.cards] or print("No more cards to draw")
 print(drawn_cards)
+
+'''
+probability of drawing a card with number 2
+'''
+number_2_cards = [card for card in deck.cards if card.number == "2"]
+simulated_2_probability = len(number_2_cards) / len(deck.cards)
+print(simulated_2_probability)
+shape_hearts = [card for card in deck.cards if card.shape == "Hearts"]
+simulated_heart_probability = len(shape_hearts) / len(deck.cards)
+print(simulated_heart_probability)
+suit_diamonds_3 = [card for card in deck.cards if card.shape == "Diamonds" and card.number == "3"]
+simulated_diamonds_of_three_probability = len(suit_diamonds_3) / len(deck.cards)
+print(simulated_diamonds_of_three_probability)
+
+# THEORITICAL PROBABILITY
+'''
+probability of drawing a card with number 2
+'''
+numbr_2_cards_theoritical = 4
+theory_2_probability = numbr_2_cards_theoritical / len(deck.cards)
+print(theory_2_probability)
+
+shape_of_heart_theoritical = 13
+theory_heart_probability = shape_of_heart_theoritical / len(deck.cards)
+print(theory_heart_probability)
+
+suit_diamonds_3_theoritical = 1
+theory_diamonds_of_three_probability = suit_diamonds_3_theoritical / len(deck.cards)
+print(theory_diamonds_of_three_probability)
 
